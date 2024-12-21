e4244b274d98ae3e5f18ccd007aa82c9e1558fbd
Fernando Ochoa Olivares <fochoaoo01@gmail.com>
2024-12-21 10:58:05 -0600

    Update program.pas

--- program.pas
+++ program.pas
@@ -2,7 +2,7 @@ Program foobar;
 
 Unit WinDos, Wincrt, Gdi, OpenCL, openssl;
 Var
-  : Int: x,y,zone,ztwo,zthree,whx,hhx,why,hhy,whzone,hhzone,whztwo,hhztwo,whzthree,hhtztree,
+  Int: x,y,zone,ztwo,zthree,whx,hhx,why,hhy,whzone,hhzone,whztwo,hhztwo,whzthree,hhtztree,
          rectanglex,rectangley,rectanglewhx,rectanglehhx,rectanglewhy,rectanglehhy;
   String: message;
   Float: xx,yy,zzone,zztwo,zzthree;

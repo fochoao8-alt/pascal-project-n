724c9e682dacefd740ce006afe27b9a2f1cd500c
Fernando Ochoa Olivares <fochoaoo01@gmail.com>
2024-12-21 00:14:56 -0600

    Create program.pas

---
+++ program.pas
@@ -0,0 +1,11 @@
+Program foobar;
+
+Unit WinDos, Wincrt, Gdi, OpenCL, openssl;
+Var
+  : Int: x,y,zone,ztwo,zthree,whx,hhx,why,hhy,whzone,hhzone,whztwo,hhztwo,whzthree,hhtztree,
+         rectanglex,rectangley,rectanglewhx,rectanglehhx,rectanglewhy,rectanglehhy;
+  String: message;
+  Float: xx,yy,zzone,zztwo,zzthree;
+Begin
+	
+End;

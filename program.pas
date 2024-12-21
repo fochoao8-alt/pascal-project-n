fb19f19f771a5706d779fa5b99d0d2a27a25a16f
Fernando Ochoa Olivares <fochoaoo01@gmail.com>
2024-12-21 05:39:02 -0600

    Update program.pas

--- program.pas
+++ program.pas
@@ -6,6 +6,13 @@ Var
          rectanglex,rectangley,rectanglewhx,rectanglehhx,rectanglewhy,rectanglehhy;
   String: message;
   Float: xx,yy,zzone,zztwo,zzthree;
+  Graphics: canvas;
+  Pen: canvaspen;
+  Brush: canvasbrush;
 Begin
-	
+  Graphics := canvas.Create(Canvas.Handle);
+  Pen := canvaspen.Create(Canvas.Create(30, 30, 30));
+  Brush := canvasbrush.Create(Canvas.Create(200, 200, 200));
+  Graphics.FillEllipse(Brush, 10, 10, 50, 50);
+  Graphics.DrawEllipse(Pen, 10, 10, 50, 50);
 End;
